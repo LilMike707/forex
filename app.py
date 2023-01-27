a245a577a071249248e318c32f06e8399e65fa92
LilMike707 <michaelmarracq@yahoo.com>
2023-01-26 18:12:46 -0800

    start

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+from forex_python.converter import CurrencyRates
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def start():
+    return render_template('index.html')
+
+
+@app.route('/convert', methods=['POST'])
+def convert():
+    value = float(request.form['amount'])
+    from_currency = request.form['from_currency']
+    to_currency = request.form['to_currency']
+    c = CurrencyRates()
+    converted_amount = c.convert(from_currency, to_currency, value)
+    return f'{value} {from_currency} is equal to {converted_amount} {to_currency}'
